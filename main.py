ada9b9d3935f80f156c28d3060668f85dd95793c
Joao Zsigmond <jzsiggy@gmail.com>
2020-06-28 01:20:20 -0300

    Fixing for raycasts

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pyglet
 from pyglet.window import Window
 from pyglet.window import key
-from pyglet.gl import *
+from pyglet import shapes
 
 import math
 
 from Player import car
+from Raycast import Raycast
 from track import track
 
+raycast = Raycast(car)
 
 window = Window(960, 700)
 window.push_handlers(car.key_handler)
@@ -15,20 +17,15 @@ window.push_handlers(car.key_handler)
 @window.event
 def on_draw():
     window.clear()
-    track.draw()
     car.draw()
+    track.draw()
 
-    # DRAW RAYCASTS
-    s = math.sin(car.angle_radians)
-    c = math.cos(car.angle_radians)
-
-    glBegin(GL_LINES)
-    glVertex2f(car.x,car.y)
-    glVertex2f(car.x + 1000*s,car.y + 1000*c)
-    glEnd()
+    for line in raycast.rays:
+        line.draw()
 
 def update(dt):
     car.update(dt)
+    raycast.update(car)
 
 if __name__ == '__main__':
     pyglet.clock.schedule_interval(update, 1/120.0)
---
+++ track.py
@@ -0,0 +1,6 @@
+import pyglet
+from pyglet import shapes
+
+track = pyglet.graphics.Batch()
+
+line2 = shapes.Line(150, 150, 444, 111, width=4, color=(200, 20, 20), batch=track)
