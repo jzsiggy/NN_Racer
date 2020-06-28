54280c5c56e8ab25b651522840ee4deb929b7ec2
Joao Zsigmond <jzsiggy@gmail.com>
2020-06-28 03:49:34 -0300

    assets

--- track.py
+++ track.py
@@ -3,4 +3,4 @@ from pyglet import shapes
 
 track = pyglet.graphics.Batch()
 
-line2 = shapes.Line(150, 150, 444, 111, width=4, color=(200, 20, 20), batch=track)
+line2 = shapes.Line(0, 0, 2000, 2000, width=4, color=(200, 20, 20), batch=track)
