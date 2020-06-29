8ce7fd0e74447ec0422bdbbacb0456834a2cdfd3
Joao Zsigmond <jzsiggy@gmail.com>
2020-06-29 14:51:51 -0300

    Creating brain from scratch

--- Brain.py
+++ Brain.py
@@ -1,5 +1,29 @@
-import tensorflow as tf
-from tensorflow import keras
+from Network import Network 
+import numpy as np
 
 class Brain():
+  def __init__(self):
+    self.network = Network([6, 5, 4])
+
+  def get_impulse(self, distances):
+    dist = np.array(distances).reshape(6, 1)
+    return self.network.feedforward(dist)
+
+# import numpy as np
+
+# import tensorflow as tf
+# from tensorflow import keras
+
+# tf.random.set_seed(2)
+
+# class Brain():
+#   def __init__(self):
+#     self.network = keras.models.Sequential([
+#       keras.layers.Dense(5, input_dim=5, activation='relu'),
+#       keras.layers.Dense(4, activation='softmax')
+#     ])
+#     self.network.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
   
+#   def get_impulse(self, distances):
+#     dist = np.array([distances])
+#     return self.network.predict(dist)
