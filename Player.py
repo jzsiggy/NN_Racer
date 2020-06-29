2648b385e777a0135ef6e9dfb48d4bf923db0959
Joao Zsigmond <jzsiggy@gmail.com>
2020-06-29 14:51:27 -0300

    hooking up acceleration with brain impulse

--- Player.py
+++ Player.py
@@ -1,7 +1,9 @@
 import math
 import pyglet
-from shapely.geometry import Point
 from pyglet.window import key
+
+from shapely.geometry import Point
+
 from assets import center_image
 
 class Player(pyglet.sprite.Sprite):
@@ -16,6 +18,8 @@ class Player(pyglet.sprite.Sprite):
         self.angle_radians = -math.radians(self.rotation)
         self.distance_traveled = 0
 
+        self.impulse = [0, 0, 0, 0]
+
         img = pyglet.image.load('bad.png')
         center_image(img)
         self.red = img
@@ -24,26 +28,57 @@ class Player(pyglet.sprite.Sprite):
         center_image(img)
         self.blue = img
 
+    def turn_left(self, dt):
+        self.rotation -= self.rotate_speed * dt
+
+    def turn_right(self, dt):
+        self.rotation += self.rotate_speed * dt
+    
+    def brake(self, dt):
+        if self.velocity > 0:
+            self.velocity -= 3 * self.thrust * dt
+        else:
+            self.velocity = 0
+    
+    def accelerate(self, dt):
+        self.velocity += self.thrust * dt
+
+    def slow_down(self, dt):
+        if self.velocity > 0:
+                self.velocity -= self.thrust * dt
+        else:
+            self.velocity = 0
+
     def update_key_pressed(self, dt):
         if self.key_handler[key.LEFT]:
-            self.rotation -= self.rotate_speed * dt
+            self.turn_left(dt)
 
         if self.key_handler[key.RIGHT]:
-            self.rotation += self.rotate_speed * dt
-       
-        if self.key_handler[key.UP]:
-            self.velocity += self.thrust * dt
-        else:
-            if self.velocity > 0:
-                  self.velocity -= self.thrust * dt
-            else:
-                self.velocity = 0
+            self.turn_right(dt)
         
         if self.key_handler[key.DOWN]:
-            if self.velocity > 0:
-                self.velocity -= 3 * self.thrust * dt
-            else:
-                self.velocity = 0
+            self.brake(dt)
+        
+        if self.key_handler[key.UP]:
+            self.accelerate(dt)
+        else:
+            self.slow_down(dt)
+
+    def update_impulses(self, dt):
+        thresh = 0.4
+        if self.impulse[0] > thresh:
+            self.turn_left(dt)
+
+        if self.impulse[1] > thresh:
+            self.turn_right(dt)
+        
+        if self.impulse[2] > thresh:
+            self.brake(dt)
+        
+        if self.impulse[3] > thresh:
+            self.accelerate(dt)
+        # else:
+        #     self.slow_down(dt)
 
     def is_out_of_bounds(self):
         center = Point(480, 350)
@@ -63,11 +98,16 @@ class Player(pyglet.sprite.Sprite):
     def update_distance_traveled(self, dt):
         self.distance_traveled += self.velocity * dt
 
+    def set_impulse(self, impulse):
+        # called from Raycaster 
+        self.impulse = impulse
+
     def update(self, dt):
         force_x, force_y = 0.0, 0.0
         self.angle_radians = -math.radians(self.rotation)
 
         self.update_key_pressed(dt)
+        self.update_impulses(dt)
         self.update_distance_traveled(dt)
 
         force_x = self.velocity * math.cos(self.angle_radians)
