a0e71e29d975bedf99db7a835cbf34a2278c93f2
Joao Zsigmond <jzsiggy@gmail.com>
2020-06-30 00:09:27 -0300

    Finding intersections on track

--- Raycaster.py
+++ Raycaster.py
@@ -7,6 +7,7 @@ import numpy as np
 from shapely.geometry import LineString
 from shapely.geometry import Point
 
+from track_points import inner, outer
 from Player import Player
 
 class Raycaster(Player):
@@ -15,6 +16,23 @@ class Raycaster(Player):
     self.rays = []
     self.intersections = []
     self.distances = []
+    self.tracklines = []
+    self.set_track_lines()
+  
+  def set_track_lines(self):
+    for point1, point2 in zip(outer[:-1], outer[1:]):
+      line = LineString([
+        (point1[0], point1[1]), 
+        (point2[0], point2[1])
+      ])
+      self.tracklines.append(line)
+
+    for point1, point2 in zip(inner[:-1], inner[1:]):
+      line = LineString([
+        (point1[0], point1[1]), 
+        (point2[0], point2[1])
+      ])
+      self.tracklines.append(line)
   
   def cast(self, rotation):
     s = math.sin( rotation )
@@ -36,64 +54,35 @@ class Raycaster(Player):
     for name, rotation in self.directions.items():
        self.rays.append( self.cast(rotation) )
 
+
   def find_intersection(self, radians):
     s = math.sin( radians )
     c = math.cos( radians )
 
-    p = Point(480, 350)
-    b1 = p.buffer(320).boundary
-    b2 = p.buffer(250).boundary
-
     l = LineString([
-      (self.x - 3000*c, self.y - 3000*s), 
-      (self.x + 3000*c, self.y + 3000*s)
-    ])
-    i1 = b1.intersection(l)
-    i2 = b2.intersection(l)
-
-    try:
-      (inner_x1, inner_y1) = i2.geoms[1].coords[0]
-    except:
-      inner_x1, inner_y1 = 0, 0
-      
-    try:
-      (inner_x2, inner_y2) = i2.geoms[0].coords[0]
-    except:
-      inner_x2, inner_y2 = 0, 0
-
-    try:
-      (outer_x1, outer_y1) = i1.geoms[1].coords[0]
-    except:
-      outer_x1, outer_y1 = 0, 0
-      
-    try:
-      (outer_x2, outer_y2) = i1.geoms[0].coords[0]
-    except:
-      outer_x2, outer_y2 = 0, 0
-
-    constraint = LineString([
       (self.x, self.y), 
       (self.x + 300*c, self.y + 300*s)
     ])
-    
-    test = [
-      (inner_x1, inner_y1),
-      (inner_x2, inner_y2),
-      (outer_x1, outer_y1),
-      (outer_x2, outer_y2)
-    ]
 
+    intersections = []
+
+    for boundry in self.tracklines:
+      i = boundry.intersection(l)
+      try:
+        (i_x, i_y) = i.x, i.y
+        intersections.append((i_x, i_y))
+      except:
+        i_x, i_y = 0, 0
+    
     position = Point(self.x, self.y)
     closest = (0, 0)
     d = 1000
 
-    for entry in test:
+    for entry in intersections:
       point = Point(entry)
-
-      if (constraint.distance(point) < 1e-8):
-        if (position.distance(point) < d):
-          d = position.distance(point)
-          closest = entry
+      if (position.distance(point) < d):
+        d = position.distance(point)
+        closest = entry
 
     circle = shapes.Circle(closest[0], closest[1], 7, color=(255, 0, 0))
     distance = d
