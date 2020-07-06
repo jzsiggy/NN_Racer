bfac8a8667f4c5e5a9909be4b40a0e4a12fd13ef
Joao Zsigmond <jzsiggy@gmail.com>
2020-07-06 01:59:44 -0300

    Implementing mating and breeding algorithm

--- Brain.py
+++ Brain.py
@@ -1,18 +1,25 @@
 from Network import Network 
 import numpy as np
+import random
 
 class Brain():
-  def __init__(self, network):
-    if network:
-      self.network = Network([8, 3])
-      self.network.weights = network.weights
-      self.network.biases = network.biases
-      self.network.tweak()
-    else:
-      self.network = Network([8, 3])
+  def __init__(self):
+    self.network = Network([10, 3])
+
+  def breed(self, net1, net2):
+    print('breeding')
+
+    for l_index, layer in enumerate(self.network.weights[0]):
+      for w_index, weight in enumerate(layer):
+        if ( random.choice((True, False)) ):
+          self.network.weights[0][l_index][w_index] = net1.weights[0][l_index][w_index]
+        else:
+          self.network.weights[0][l_index][w_index] = net2.weights[0][l_index][w_index]
+
+    self.network.tweak()
 
   def get_impulse(self, distances):
-    dist = np.array(distances).reshape(8, 1)
+    dist = np.array(distances).reshape(10, 1)
     return self.network.feedforward(dist)
 
 # import numpy as np
